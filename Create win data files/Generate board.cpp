8e57ff3b6488ab05a39f43e77226353c6fac74e5
isaacp73 <1045302@my.scccd.edu>
2026-04-26 14:18:30 -0700

    -Created to generate board.cpp to print what the board looks like based on turn and position numbers.

--- Create win data files/Generate board.cpp
+++ Create win data files/Generate board.cpp
@@ -1,7 +1,25 @@
 #include "Game.h"
+#include <iostream>
 
-void moveSequence(int pos, int *moves) {
+void moveSequence(int turn, unsigned long long int pos, int *moves) {
+    int *moves = new int[turn + 1];
 
+    bool bot = true;
+
+    moves[0] = 4;
+
+    int P;
+    unsigned long long int nextPos;
+    unsigned long long int currPos = pos;
+
+    for (int i = turn; i > 0; i--) { //determine the series of moves
+        P = currPos / 7;
+        currPos % 7 != 0 ? nextPos = P + 1 : nextPos = P;
+
+        moves[i] = (currPos - (nextPos * 7 - 6)) + 1;
+
+        currPos = nextPos;
+    }
 }
 
 /*
@@ -11,5 +29,19 @@ void moveSequence(int pos, int *moves) {
 int main(char* arg[]) {
     Game board;
 
-    
+    int *moves;
+
+    moveSequence(std::stoi(arg[1]), std::stoi(arg[2]), moves);
+
+    bool which = true;
+    for (int i = 0; i < std::stoi(arg[1]); i++) {
+        board.makeMove(moves[i], which);
+        which = !which;
+    }
+
+    board.printBoard();
+
+    delete[] moves;
+
+    return 0;
 }
