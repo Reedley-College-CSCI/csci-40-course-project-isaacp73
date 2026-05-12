57e5eaf5dd4ee6de29b12b8dfa83b244f8c120d5
isaacp73 <1045302@my.scccd.edu>
2026-05-12 09:50:38 -0700

    -Cleaned up if statments in oneMoveWins function in Bot algorithms.cpp.

--- The Game/Bot algorithms.cpp
+++ The Game/Bot algorithms.cpp
@@ -7,26 +7,17 @@ bool Bot::oneMoveWins(int playerOrBot) { //1 is bot 2 is player
     for (int y = 0; y < 6; y++) { //incrament through every row
         int inARow  = 0;
         for (int x = 0; x < 6; x++) { //increment accross the row
-            if (board[x][y] == playerOrBot) { //check if theres is a piece
-                if (board[x][y] == board[x + 1][y]) { //if that piece matches the one next to it
-                    inARow++;
-                    if (inARow == 2) { //if there has been 2 times where they have matched that means there are two in a row
-                        if (x-2 >= 0) { //check left bound
-                            if (board[x - 2][y] == 0) { //check if its an open space to the left
-                                move = x-1;
-                                return true;
-                            }
-                        }
-                        if (x+2 < 7) { //check right bound
-                            if (board[x+2][y] == 0) { //check if its an open space to the right
-                                move = x + 3;
-                                return true;
-                            }
-                        }
+            if (board[x][y] == playerOrBot && board[x][y] == board[x + 1][y]) { //check if theres is a piece and it matches one next to it
+                inARow++;
+                if (inARow == 2) { //if there has been 2 times where they have matched that means there are two in a row
+                    if (x-2 >= 0 && board[x - 2][y] == 0) { //check left bound and if its an open space to the left
+                        move = x-1;
+                        return true;
                     }
-                }
-                else { //if there is a differtent piece blocking the win
-                    inARow = 0; //reset counter to 0
+                    if (x+2 < 7 && board[x+2][y] == 0) { //check right bound and if its an open space to the right
+                        move = x + 3;
+                        return true;
+                    }
                 }
             }
             else { //catches if there is a blank space blocking the win
@@ -39,20 +30,13 @@ bool Bot::oneMoveWins(int playerOrBot) { //1 is bot 2 is player
     for (int x = 0; x < 7; x++) { //incrament through every column
         int inAColumn  = 0;
         for (int y = 0; y < 5; y++) { //incrament down the column
-            if (board[x][y] == playerOrBot) { //check if there is a piece
-                if (board[x][y] == board[x][y + 1]) { //check if that piece matches the one under it
-                    inAColumn++;
-                    if (inAColumn == 2) { //if there has been 2 times where they have matched that means there are three in a column
-                        if (y-2 >= 0) { //check upper bound
-                            if (board[x][y-2] == 0) {
-                                move = x+1;
-                                return true;
-                            }
-                        }
+            if (board[x][y] == playerOrBot && board[x][y] == board[x][y + 1]) { //check if there is a piece and if that piece matches the one under it
+                inAColumn++;
+                if (inAColumn == 2) { //if there has been 2 times where they have matched that means there are three in a column
+                    if (y-2 >= 0 && board[x][y-2] == 0) { //check if its clear on top
+                        move = x+1;
+                        return true;
                     }
-                }
-                else { //if there is a differtent piece blocking the win
-                    inAColumn = 0; //reset counter to 0
                 }
             }
             else {
